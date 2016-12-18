ec4d7442364a870bda9a3164d11ff89b87a808da
Michael Müller <mg.mueller@posteo.net>
2016-12-18 17:05:43 +0100

    add scanning for access points

--- wpa_connect.sh
+++ wpa_connect.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 #
-# wpa_connect.sh - manage network connections
+# wpa_connect.sh - manage wireless network connections
 #
 # usage:
-#   - edit file: specify correct interface and wpa profile directory
+#   - edit file to specify correct interface and wpa profile directory
 #   - create wpa_supplicant profile files, e.g. using wpa_passphrase, save as
 #       profile_name.conf in wpa profile directory
-#   - run ``wpa_connect.sh profile_name'' (without .conf)
+#   - run ``wpa_connect.sh profile_name'' (without .conf) or simply
+#     ``wpa_connect.sh profile_name'' to scan for available access points
+#
+# requirements: wpa_supplicant, dhcpcd, dialog
 #
 
 IFACE=wlp2s0
@@ -14,13 +17,13 @@ PROFILE_DIR=/home/user/wpa/
 
 function usage()
 {
-  echo "$0 profile [interface]"
+  echo "$0 [profile [interface]]"
 }
 
 function disconnect()
 {
   dhcpcd -k "$IFACE"
-  [ -n "$wpa_pid" ] && kill -s TERM $wpa_pid
+  [ -n "$WPA_PID" ] && kill -s TERM $WPA_PID
   ifconfig "$IFACE" down
 }
 
@@ -31,25 +34,115 @@ function clean()
   exit 0
 }
 
-if [ -z "$1" ] ; then
-  usage
-  exit -1
-fi
-
 if [ ! -z "$2"] ; then
   IFACE="$2"
 fi
 
-profile="$1.conf"
-file="$PROFILE_DIR/$profile"
+if [ ! -z "$1" ] ; then
+  PROFILE_NAME="$1"
+  PROFILE="${PROFILE_NAME}.conf"
+else
+  # scan for available networks
 
-if [ ! -f "$file" ] ; then
-  echo "$0: could not find profile $profile in $PROFILE_DIR."
+  # start interface
+  ifconfig $IFACE down
+  ifconfig $IFACE up
+
+  echo "scanning..."
+
+  APS=$(iwlist $IFACE scan | egrep 'ESSID:"[^"]*"' | sed 's/\s*ESSID:"\([^"]*\)"\s*/\1/')
+
+  AP_COUNT=$(echo "$APS" | wc -l)
+
+  if [ $AP_COUNT -eq 0 ]; then
+    echo "no network found."
+    exit -1
+  else
+    echo "found ${AP_COUNT} networks, essids:"
+
+    # check for essids in existing profiles
+    FOUND_ESSIDS=""
+    FOUND_PROFILES=""
+    FOUND_COUNT=0
+
+    list="$APS"
+
+    while [ ! -z "$list" ]; do
+      cur="${list%% *}"
+      list="${list#$cur}"
+      list="${list# }"
+
+      echo "  $cur"
+
+      for f in ${PROFILE_DIR}/* ; do
+        profile=$(basename $f)
+        profile=${profile%.conf}
+        ssid=$(egrep 'ssid="[^"]*"' $f | sed 's/\s*ssid="\([^"]*\)"\s*/\1/')
+
+        if [ "$cur" != "$ssid" ]; then
+          continue
+        fi
+
+        # profile found, add to list
+
+        if [ -z "${FOUND_PROFILES}" ]; then
+          FOUND_PROFILES="$profile"
+        else
+          FOUND_PROFILES="${FOUND_PROFILES} $profile"
+        fi
+
+        if [ -z "${FOUND_ESSIDS}" ]; then
+          FOUND_ESSIDS="$ssid"
+        else
+          FOUND_ESSIDS="${FOUND_ESSIDS} $ssid"
+        fi
+
+        FOUND_COUNT=$(expr $FOUND_COUNT + 1)
+      done
+
+    done
+
+    if [ $FOUND_COUNT -eq 1 ]; then
+      echo "found 1 network with existing profile"
+      PROFILE_NAME="$FOUND_PROFILES"
+    else
+      echo "found ${PROFILE_COUNT} networks with existing profile"
+
+      list="${FOUND_PROFILES}"
+
+      EXEC_STR="dialog --menu \"select network:\" 0 0 0 "
+
+      while [ ! -z "$list" ]; do
+        cur="${list%% *}"
+        list="${list#$cur}"
+        list="${list# }"
+
+        EXEC_STR="${EXEC_STR} \"$cur\" \"\""
+      done
+
+      EXEC_STR="${EXEC_STR} 3>&1 1>&2 2>&3"
+
+      opt=$(eval ${EXEC_STR})
+      [ -z "$opt" ] && exit -1  # aborted
+
+      PROFILE_NAME="$opt"
+    fi
+  fi
+fi
+
+
+# connect to selected profile
+
+PROFILE="${PROFILE_NAME}.conf"
+PROFILE_FILE="$PROFILE_DIR/$PROFILE"
+
+if [ ! -f "${PROFILE_FILE}" ] ; then
+  echo "$0: could not find profile $PROFILE in $PROFILE_DIR."
   exit -1
 fi
 
 echo "using interface $IFACE"
-echo "using profile $1"
+echo "using profile ${PROFILE_NAME}"
 
 
 # check priviledges
@@ -61,8 +154,8 @@ fi
 
 # copy profile
 echo "copy profile"
-wpa_file="/etc/wpa_supplicant/wpa_supplicant-$IFACE.conf"
-cp -v $file $wpa_file
+WPA_FILE="/etc/wpa_supplicant/wpa_supplicant-$IFACE.conf"
+cp -v ${PROFILE_FILE} ${WPA_FILE}
 
 
 # catch CTRL-C etc.
@@ -77,8 +170,8 @@ while true ; do
   ifconfig $IFACE up
   
   # authenticate
-  wpa_supplicant -i $IFACE -c $wpa_file -qq &
-  wpa_pid=$!
+  wpa_supplicant -i $IFACE -c ${WPA_FILE} -qq &
+  WPA_PID=$!
 
   # wait for authentication to complete
   sleep 2
@@ -105,3 +198,4 @@ while true ; do
   # release ip and kill wpa_supplicant
   disconnect
 done
+
